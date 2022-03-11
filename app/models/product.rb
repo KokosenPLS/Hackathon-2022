146854700862dfb6bf0ce5a33578aa8a70a44da6
Alexander <alexanderlongumolsen@gmail.com>
2022-03-11 22:15:58 +0100

    innit commit

---
+++ app/models/product.rb
@@ -0,0 +1,30 @@
+class Product < ApplicationRecord
+
+  has_one_attached :image
+  has_many :comments
+
+  validate :acceptable_image
+  after_commit :add_default_cover, on: [:create, :update]
+
+
+  def acceptable_image
+    if image.attached?
+      acceptable_types = ["image/jpeg", "image/png"]
+
+      unless acceptable_types.include?(image.content_type)
+        errors.add(:main_image, "must be a JPEG or PNG")
+      end
+
+    end
+  end
+
+  private
+
+  def add_default_cover
+    unless image.attached?
+      self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default.png")), filename: 'default.png' , content_type: "image/png  ")
+    end
+  end
+
+
+end
